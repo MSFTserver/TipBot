56b7951901e4447dd5879f61ce08e80a3eb00a3c
MSFTserver <sawyerlemmo@gmail.com>
2018-06-03 21:32:51 -0700

    add days to uptime

--- bot/modules/bot-uptime.js
+++ bot/modules/bot-uptime.js
@@ -12,12 +12,14 @@ exports.uptime = {
     }
     msg.channel.send(
       'i have been Online for ' +
-        Math.round(bot.uptime / (1000 * 60 * 60)) +
-        ' hours, ' +
-        Math.round(bot.uptime / (1000 * 60)) % 60 +
-        ' minutes, and ' +
-        Math.round(bot.uptime / 1000) % 60 +
-        ' seconds'
+      Math.round(bot.uptime / (1000 * 60 * 60 * 24)) +
+      ' days, ' +
+      Math.round(bot.uptime / (1000 * 60 * 60)) +
+      ' hours, ' +
+      Math.round(bot.uptime / (1000 * 60)) % 60 +
+      ' minutes, and ' +
+      Math.round(bot.uptime / 1000) % 60 +
+      ' seconds'
     );
   }
 };
